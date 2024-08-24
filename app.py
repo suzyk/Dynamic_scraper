63834a58a93f530a07401624d54f5cfeff612a82
suzyk <suzykahng@gmail.com>
2024-08-24 12:23:32 +0300

    refactored code for testing mode

--- app.py
+++ app.py
@@ -50,73 +50,71 @@ def scrape_wanted_jobs():
     save_to_file(f"wanted_{key}", jobs_db)
   p.stop()
 
-def scrape_remoteok_jobs():
+def scrape_remoteok_jobs(testing=False):
+  
+  def parse_html(content):
+    soup = BeautifulSoup(content, "html.parser")
+    jobs = soup.find("table", id = "jobsboard").find_all("tr", class_ = "job")
+
+    for job in jobs:
+        title = job.find("h2").text
+        company = job.find("h3").text
+
+        region_salary = job.find_all("div", class_ = "location")
+        region = ""
+        salary = ""
+    
+        if len(region_salary) > 0 :
+            all_regions = []
+            for i in region_salary:# everything except last item
+                if "$" in i.text:
+                    salary = i.text
+                    break  # do not care after salary info
+                else:
+                    all_regions.append(i.text)
+            region = ", ".join(all_regions)  
+        
+        region = "No region" if len(region) == 0 else region
+        salary = "No salary info" if len(salary) == 0 else salary
+    
+        """
+        try:
+            salary = job.find('div', class_='location tooltip').text.strip()
+        except AttributeError:
+            salary = 'no data'
+        """
+        #another way
+        #url = job.find("a", itemprop="url")["href"]
+        url = job.find("a", class_ = "preventLink")["href"]
+        jobs_db.append(Job(title.strip(), company.strip(), f"https://remoteok.com{url}", region.strip(), salary.strip()))
+        
   def scrape_jobs(url):
     jobs_db.clear()
-    r = requests.get(url, 
+    if not testing:
+      r = requests.get(url, 
                    headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"})
 
-    if (r.status_code == 200):
-        soup = BeautifulSoup(r.content, "html.parser")
-        jobs = soup.find("table", id = "jobsboard").find_all("tr", class_ = "job")
-    
-        for job in jobs:
-            title = job.find("h2").text
-            company = job.find("h3").text
-
-            region_salary = job.find_all("div", class_ = "location")
-            region = ""
-            salary = ""
-        
-            if len(region_salary) > 0 :
-                all_regions = []
-                for i in region_salary:# everything except last item
-                    if "$" in i.text:
-                        salary = i.text
-                        break  # do not care after salary info
-                    else:
-                        all_regions.append(i.text)
-                region = ", ".join(all_regions)  
-            
-            region = "No region" if len(region) == 0 else region
-            salary = "No salary info" if len(salary) == 0 else salary
-        
-            """
-            try:
-                salary = job.find('div', class_='location tooltip').text.strip()
-            except AttributeError:
-                salary = 'no data'
-            """
-            #another way
-            #url = job.find("a", itemprop="url")["href"]
-            url = job.find("a", class_ = "preventLink")["href"]
-            jobs_db.append(Job(title.strip(), company.strip(), f"https://remoteok.com{url}", region.strip(), salary.strip()))
-    else:
+      if (r.status_code == 200):
+        parse_html(r.content)
+      else:
         print(f"request error with {r.content}")
+    else:
+      f = open(url, encoding="utf8")
+      parse_html(f)
+      f.close()
 
   for key in keywords:
-    url = f"https://remoteok.com/remote-{key}-jobs"
+    if not testing:
+      url = f"https://remoteok.com/remote-{key}-jobs"
+    else:
+       url = f"remoteok_{key}.html"
     scrape_jobs(url)
     if len(jobs_db) > 0:
       save_to_file(f"remoteok_{key}", jobs_db)
 
-def scrape_weworkremotely():
+def scrape_weworkremotely(testing=False):
 
-  def scrape_page(url):
-    """
-    response = requests.get(url,
-                            headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"})
-    
-    content = response.content
-    """
-    response = None
-    # read local file if access is defied
-    if response == None or response.status_code != 200 :
-      f = open("weworkremotely.html", encoding="utf8")
-      content = f
-      
-      print("reading local file")
-      
+  def parse_html(content):
     soup = BeautifulSoup(content, "html.parser")
 
     #used to be category-2
@@ -141,21 +139,27 @@ def scrape_weworkremotely():
         """
       #url = job.find("a")["href"] # this is how to extract attribute
       url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"] 
-      job_data = {
-        "title": title,
-        "company": company.text,
-        "position": position.text,
-        "region": "No Region" if not region else region[0].text,
-        "url": f"https://weworkremotely.com{url}"
-      }
       url = f"https://weworkremotely.com{url}"
       #position is full-time/contract info
-      region = "No Region" if not region else region[0].text
+      region = region[0].text if region else None #"No Region" if not region else region[0].text
       jobs_db.append(Job(title = title, 
                          company = company.text.strip(), 
                          url = url, 
                          region = region, 
                          position = position.text.strip()))
+      
+  def scrape_page(url):
+    if not testing:
+      r = requests.get(url,
+                            headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"})
+    
+      if r.status_code == 200:
+        parse_html(r.content)
+      else:
+        print(f"request error with {r.content}")
+    else:
+      f = open(url, encoding="utf8")
+      parse_html(f)
       f.close()
     
   def get_pages(url):
@@ -167,20 +171,19 @@ def scrape_weworkremotely():
     else:
        raise print(f"Cannot access the site ({response.status_code})")
 
-  url = "https://weworkremotely.com" #/categories/remote-full-stack-programming-jobs" #remote-full-time-jobs"
-  """
-  pages = get_pages(url)
-  for page in range(pages):
-    url = f"https://weworkremotely.com/remote-full-time-jobs?page={page+1}"
-    scrape_page(url)
-  """
-  scrape_page(url)
-  if len(jobs_db) > 0:
-    save_to_file("weworkremotely", jobs_db)
+  if not testing :
+    pages = get_pages("https://weworkremotely.com/remote-full-time-jobs")
+    for page in range(pages):
+      url = f"https://weworkremotely.com/remote-full-time-jobs?page={page+1}"
+      scrape_page(url)
+      save_to_file("weworkremotely", jobs_db) 
+  else:
+      scrape_page("weworkremotely.html")
+      save_to_file("weworkremotely", jobs_db) 
 
 #scrape_weworkremotely() ## request issues
 
 #scrape_wanted_jobs()
-#scrape_remoteok_jobs()
-scrape_weworkremotely()
+#scrape_remoteok_jobs(True)
+#scrape_weworkremotely(True)
 
--- file.py
+++ file.py
@@ -5,7 +5,6 @@ def save_to_file(file_name, jobs):
     file = open(f"{file_name}_jobs.csv", "w")
     writer = csv.writer(file)
     writer.writerow(Job.get_parameters())
-    #writer.writerow("Title","Company","Url","Position","Region","Salary")
     for job in jobs:
         writer.writerow(job.get_values())
     file.close()
